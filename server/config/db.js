8d012280ef3fab04677738cb8e3a208fa1674e38
Vishal Tiwari <vk976082@gmail.com>
2025-10-15 10:10:46 +0530

    usefull commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
-const connectdb = async ()=>{
-    try{
-        mongoose.connection.on('connected', ()=>{
+const connectdb = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
             console.log("database connected");
         })
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+      const conn =  await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}/${conn.connection.name}`);
 
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
-        
+
 
     }
 }
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,20 +2,22 @@ import { Inngest } from "inngest";
 import User from "../models/user.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "movie-ticket-booki" });
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 //inngest fn to dsave userdata in db
 const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-from-clerk" },
-  { event: "clerk/user.created" },
+   { id: "sync_user_from_clerk" },
+  { event: 'clerk/user.created' },
   async ({ event }) => {
-    const { id, first_name, last_name, email_address, image_url } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
     const userData = {
       _id: id,
-      email: email_address[0],
-      name: first_name + " " + last_name,
+      email: email_addresses[0].email_address,          // full email
+      name: `${first_name} ${last_name}`,
       image: image_url,
     };
+    console.log(userData)
     await User.create(userData);
   }
 );
